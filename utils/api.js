1d7a756b32030275b3e4b8ee040c4a019624cb1d
Arman <mesropyanarman4@gmail.com>
2023-05-29 13:47:18 +0400

    api changes

--- utils/api.js
+++ utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_HOST = process.env.API_HOST;
 
 const instance = axios.create({
-  baseURL: `${API_HOST}`,
+  baseURL: `${API_HOST}/api`,
 });
 
 export const fetchDataFromApi = async (endpoint) => {
